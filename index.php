e9c9a9ed21f828c2be323d84120becfffa8f6db6
Afinda <afinda.nuraji@gmail.com>
2024-05-20 16:46:02 +0800

    new question

--- index.php
+++ index.php
@@ -5,16 +5,35 @@
     </head>
     <body>
         <?php
-            $name = "Muahmmad Asyraf";
+            $name = "Nur Afinda Audina binti Muraji";
+            $matric_no = "AI220146";
+            $course = "BIT";
+            $year = "3.5 years";
+            $address = "Kolej Kediaman Tun Fatimah, UTHM";
+
         ?>
 
         <table>
             <tr>
-                <td>Name</td>
+                <td>Name:   </td>
                 <td><?php echo "$name"; ?></td>
+           <tr>
+                <td>Matric number:  </td>
+                <td><?php echo "$matric_no"; ?></td>
+            </tr>
+            <tr>
+                <td>Course: </td>
+                <td><?php echo "$course"; ?></td>
+            </tr>
+                <td>Years of study: </td>
+                <td><?php echo "$year"; ?></td>
+            <tr>
+                <td>Address:    </td>
+                <td><?php echo "$address"; ?></td>
             </tr>
         </table>
 
 </body>
 </html>
+
 
