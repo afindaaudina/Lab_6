26d6f05f3e7cfb0f0bd866c005f434dd188c3369
Afinda <afinda.nuraji@gmail.com>
2024-05-20 15:34:06 +0800

    new

---
+++ Lab6_q1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> Lab 6 Q1 </title>
+    </head>
+    <body>
+        <?php
+            $name = "Muahmmad Asyraf";
+        ?>
+
+        <table>
+            <tr>
+                <td>Name</td>
+                <td><?php echo "$name"; ?></td>
+            </tr>
+        </table>
+
+</body>
+</html>
+
